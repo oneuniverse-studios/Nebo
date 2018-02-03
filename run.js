e66b926e7b37bcff8e1d370020a8e9131059039d
zPr0D3v <36106780+zPr0D3v@users.noreply.github.com>
2018-02-03 14:16:07 -0600

    Update run.js

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 
-const config = require("./config.json");
+const prefix = "nebo, "
 
 // This loop reads the /events/ folder and attaches each event file to the appropriate event.
 fs.readdir("./events/", (err, files) => {
@@ -16,9 +16,9 @@ fs.readdir("./events/", (err, files) => {
 });
 
 client.on("message", message => {
-  if (message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
-
+// Exit and stop if the prefix is not there or if user is a bot
+  const teamCreate = message.content.toLowerCase();
+if (!teamCreate.startsWith(prefix) || message.author.bot || message.channel.type === "dm") return;
   // This is the best way to define args. Trust me.
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
